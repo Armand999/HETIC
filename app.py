8eaf0ebef9c0b2edb79852b8e2b7e2e2a4d71962
Armand999 <armandkou@live.fr>
2022-05-03 12:01:10 +0200

    mise à jour

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask import abort
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World from Python'
+
+
+if __name__ == "__main__":
+  app.run()
